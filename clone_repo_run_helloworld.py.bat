50f6b653560c28bb9745ca42d1172b3712f567ed
J <justintime155@gmail.com>
2022-07-11 03:40:14 -0400

    baby steps

---
+++ clone_repo_run_helloworld.py.bat
@@ -0,0 +1,11 @@
+set repo_path=https://github.com/QAScavTech/scripts_repo
+echo %repo_path%
+
+git clone %repo_path%
+
+rem type rem to make a comment and script wont read it
+
+cd scripts_repo
+
+python helloworld.py
+pause
